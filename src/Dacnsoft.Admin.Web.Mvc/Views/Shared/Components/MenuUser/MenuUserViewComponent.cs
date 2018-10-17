439a67b7762be6ea4b6430e14aa2f098dbde9e85
Gerry Ge <99432281@qq.com>
2018-10-17 12:38:39 +0800

    add admin-lte view components

---
+++ src/Dacnsoft.Admin.Web.Mvc/Views/Shared/Components/MenuUser/MenuUserViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dacnsoft.Admin.Web.Views.Shared.Components.MenuUser
+{
+    public class MenuUserViewComponent : ViewComponent
+    {
+
+        public MenuUserViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke(string filter)
+        {
+            return View();
+        }
+    }
+}
