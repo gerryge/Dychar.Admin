5854989a56a45aeef986895e469975c238d5b2b1
Gerry Ge <99432281@qq.com>
2018-10-19 12:12:51 +0800

    Enhance the main menu components

--- src/Dacnsoft.Admin.Web.Mvc/Startup/PageNames.cs
+++ src/Dacnsoft.Admin.Web.Mvc/Startup/PageNames.cs
@@ -6,6 +6,15 @@
         public const string About = "About";
         public const string Tenants = "Tenants";
         public const string Users = "Users";
-        public const string Roles = "Roles"; 
+        public const string Roles = "Roles";
+        public const string Test1 = "Test1";
+        public const string Test2 = "Test3";
+        public const string Test3 = "Test3";
+        public const string Test4 = "Test4";
+        public const string Test5 = "Test5";
+        public const string Test6 = "Test6";
+        public const string Test7 = "Test7";
+        public const string Test8 = "Test8";
+        public const string Test9 = "Test9";
     }
 }
